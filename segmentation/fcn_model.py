ed20dbb6ba9e2103a3a48349f6df159b10f026a4
Isabelle Bouchard <isabelle@elementai.com>
2018-05-29 21:13:38 +0000

    improve fcn model

--- segmentation/fcn_model.py
+++ segmentation/fcn_model.py
@@ -1,12 +1,8 @@
+import torch
 import torch.nn as nn
 from torch import randn
 import torch.nn.functional as F
 from torchvision.models import vgg16
-
-import torchvision.transforms as transforms
-from torchvision.transforms import ToTensor
-from torch.autograd import Variable
-
 
 class FCNModel(nn.Module):
     def __init__(self):
@@ -14,8 +10,12 @@ class FCNModel(nn.Module):
 
         # VGG16 pretrained features
         VGG16 = vgg16(pretrained=True)
-        self.features = VGG16.features
-        print('vgg16')
+        # Separate VGG16 after pool layers to connect the skip connections
+        self.block_pool0 = VGG16.features[:5]
+        self.block_pool1 = VGG16.features[5:10]
+        self.block_pool2 = VGG16.features[10:17]
+        self.block_pool3 = VGG16.features[17:24]
+        self.block_pool4 = VGG16.features[25:]
 
         self.conv1 = nn.Conv2d(512, 4096, 7)
         self.conv2 = nn.Conv2d(4096, 4096, 1)
@@ -24,40 +24,73 @@ class FCNModel(nn.Module):
 
         self.deconv1 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
         self.crop = nn.ConstantPad2d(-2, -2)
-        self.deconv2 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.deconv3 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.deconv4 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.deconv5 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.deconv6 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.deconv7 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.deconv8 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.deconv9 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(533, 21, 4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(533, 21, 4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(277, 21, 4, stride=2, padding=1)
+        self.deconv5 = nn.ConvTranspose2d(149, 21, 4, stride=2, padding=1)
+        self.deconv6 = nn.ConvTranspose2d(85, 21, 4, stride=2, padding=1)
 
     def forward(self, x):
-        input_size = x.size()
-        print(input_size)
-        x = self.features(x)
         print(x.size())
-        x = self.conv1(x)
+        out_pool0 = self.block_pool0(x)
+        print('0')
+        print(out_pool0.size())
+        out_pool1 = self.block_pool1(out_pool0)
+        print('1')
+        print(out_pool1.size())
+        out_pool2 = self.block_pool2(out_pool1)
+        print('2')
+        print(out_pool2.size())
+        out_pool3 = self.block_pool3(out_pool2)
+        print('3')
+        print(out_pool3.size())
+        out_pool4 = self.block_pool4(out_pool3)
+        print('4')
+        print(out_pool4.size())
+        x = F.relu(self.conv1(out_pool4))
+        print('5')
         print(x.size())
-        x = self.conv2(x)
+        x = F.relu(self.conv2(x))
+        print('6')
         print(x.size())
-        x = self.conv3(x)
+        x = F.relu(self.conv3(x))
+        print('7')
         print(x.size())
-        x = self.conv4(x)
+        x = F.relu(self.conv4(x))
+        print('8')
         print(x.size())
-        x = self.deconv1(x)
+        x = F.relu(self.deconv1(x))
+        print('9')
         print(x.size())
         x = self.crop(x)
+        print('10')
+        x = torch.cat((x, out_pool4), 1)
         print(x.size())
-        x = self.deconv2(x)
+        x = F.relu(self.deconv2(x))
+        print('11')
+        x = torch.cat((x, out_pool3), 1)
         print(x.size())
-        x = self.deconv3(x)
+        x = F.relu(self.deconv3(x))
+        print('12')
+        x = torch.cat((x, out_pool2), 1)
         print(x.size())
-        x = self.deconv4(x)
+        x = F.relu(self.deconv4(x))
+        print('13')
+        x = torch.cat((x, out_pool1), 1)
         print(x.size())
-        x = self.deconv5(x)
+        x = F.relu(self.deconv5(x))
+        print('14')
+        x = torch.cat((x, out_pool0), 1)
         print(x.size())
-        x = self.deconv6(x)
+        x = F.relu(self.deconv6(x))
+        print('15')
         print(x.size())
         return x
+
+    def from_file(path):
+        self.load_state_dict(torch.load(path))
+
+if __name__ == '__main__':
+    model = FCNModel()
+    x = randn(1, 3, 512, 512)
+    model.forward(x)
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from segmentation.fcn_model import FCNModel
 from segmentation.datasets import PascalVOC2012, CIFAR
 
 
-def train(model, dataset, criterion, optimizer, n_epoch=2):
+def train(model, dataset, criterion, optimizer, path='/project/fcn.pt', n_epoch=2):
     for epoch in range(n_epoch):
 
         running_loss = 0.0
@@ -26,13 +26,15 @@ def train(model, dataset, criterion, optimizer, n_epoch=2):
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 200 == 199:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
+                    (epoch + 1, i + 1, running_loss / 200))
                 running_loss = 0.0
 
     print('Finished Training')
 
+    torch.save(model.state_dict(), path)
+    print('Model saved: {}'.format(path))
 
 
 if __name__ == '__main__':
@@ -43,4 +45,4 @@ if __name__ == '__main__':
     dataset = PascalVOC2012('train')
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    train(model, dataset, criterion, optimizer)
+    train(model, dataset, criterion, optimizer, n_epoch=1)
