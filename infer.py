ca414ab6f6b56b0bde93b9be2c86483cdf60e7c5
Isabelle Bouchard <isabelle@elementai.com>
2018-05-31 14:39:24 +0000

    improved model

--- infer.py
+++ infer.py
@@ -11,30 +11,32 @@ from segmentation.datasets import PascalVOC2012
 
 
 def infer(model, dataset, count=1, random=True):
+    nonnull = 0
     dataloader = DataLoader(dataset, batch_size=1, shuffle=random)
-    for i in range(count):
-        data = dataloader[i]
+    for i, data in enumerate(dataloader):
         result = model(data['image'])
 
         out = result.detach().numpy()[0].argmax(axis=0)
         _min, _max = out.min(), out.max()
-        print('{}: min = {}, max = {}'.format(i, _min, _max)
+        print('{}: min = {}, max = {}'.format(i, _min, _max))
 
         if _max != 0:
             nonnull += 1
-    print('{} out of {} gave something'.format(nonnull, count))
 
+        if i == count -1:
+            print('{} out of {} gave something'.format(nonnull, count))
+            return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', help='Experiment number')
-    parser.add_argument('--count', help='Inference count')
-    parser.add_argument('--random', help='Pick random image from valid set')
+    parser.add_argument('--count', type=int, help='Inference count')
+    parser.add_argument('--random', type=bool, help='Pick random image from valid set')
 
     args = parser.parse_args()
 
     model = FCNModel(vgg16_pretrained=False)
-    mod.from_file('/project/fcn_{}.pt'.format(args.n))
+    model.from_file('/project/fcn_{}.pt'.format(args.n))
 
     dataset = PascalVOC2012('val')
 
--- segmentation/fcn_model.py
+++ segmentation/fcn_model.py
@@ -11,49 +11,61 @@ class FCNModel(nn.Module):
         # VGG16 pretrained features
         VGG16 = vgg16(pretrained=vgg16_pretrained)
         # Separate VGG16 after pool layers to connect the skip connections
-        self.block_pool0 = VGG16.features[:5]
-        self.block_pool1 = VGG16.features[5:10]
-        self.block_pool2 = VGG16.features[10:17]
-        self.block_pool3 = VGG16.features[17:24]
-        self.block_pool4 = VGG16.features[25:]
+        self.vgg_pool1 = VGG16.features[:5]
+        self.vgg_pool2 = VGG16.features[5:10]
+        self.vgg_pool3 = VGG16.features[10:17]
+        self.vgg_pool4 = VGG16.features[17:24]
+        self.vgg_pool5 = VGG16.features[25:]
 
+        # Fully conv
         self.conv1 = nn.Conv2d(512, 4096, 7)
+        # TODO: add dropout
         self.conv2 = nn.Conv2d(4096, 4096, 1)
-        self.conv3 = nn.Conv2d(4096, 1000, 1)
-        self.conv4 = nn.Conv2d(1000, 21, 1)
+        # TODO: add dropout
+        self.conv3 = nn.Conv2d(4096, 21, 1)
+        self.deconv1 = nn.ConvTranspose2d(21, 21, 4, stride=2)
 
-        self.deconv1 = nn.ConvTranspose2d(21, 21, 4, stride=2, padding=1)
-        self.crop = nn.ConstantPad2d(-2, -2)
-        self.deconv2 = nn.ConvTranspose2d(533, 21, 4, stride=2, padding=1)
-        self.deconv3 = nn.ConvTranspose2d(533, 21, 4, stride=2, padding=1)
-        self.deconv4 = nn.ConvTranspose2d(277, 21, 4, stride=2, padding=1)
-        self.deconv5 = nn.ConvTranspose2d(149, 21, 4, stride=2, padding=1)
-        self.deconv6 = nn.ConvTranspose2d(85, 21, 4, stride=2, padding=1)
+        self.conv_0 = nn.Conv2d(512, 21, 1, padding=0)
+        # TODO add correct padding value
+        self.crop_0 = nn.ZeroPad2d(-5)
+        self.deconv_0 = nn.ConvTranspose2d(21, 21, 4, stride=2)
 
+        self.conv_1 = nn.Conv2d(256, 21, 1, padding=0)
+        # TODO add correct padding value
+        self.crop_1 = nn.ZeroPad2d(-9)
+        self.deconv_1 = nn.ConvTranspose2d(21, 21, 16, stride=11)
+
+        self.crop_f = nn.ZeroPad2d((1, 0, 1, 0))
         self.softmax = nn.Softmax()
 
     def forward(self, x):
-        out_pool0 = self.block_pool0(x)
-        out_pool1 = self.block_pool1(out_pool0)
-        out_pool2 = self.block_pool2(out_pool1)
-        out_pool3 = self.block_pool3(out_pool2)
-        out_pool4 = self.block_pool4(out_pool3)
-        x = F.relu(self.conv1(out_pool4))
+        # VGG 16
+        out_pool1 = self.vgg_pool1(x)
+        out_pool2 = self.vgg_pool2(out_pool1)
+        out_pool3 = self.vgg_pool3(out_pool2)
+        out_pool4 = self.vgg_pool4(out_pool3)
+        out_pool5 = self.vgg_pool5(out_pool4)
+
+        # Fully conv
+        x = F.relu(self.conv1(out_pool5))
         x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = F.relu(self.deconv1(x))
-        x = self.crop(x)
-        x = torch.cat((x, out_pool4), 1)
-        x = F.relu(self.deconv2(x))
-        x = torch.cat((x, out_pool3), 1)
-        x = F.relu(self.deconv3(x))
-        x = torch.cat((x, out_pool2), 1)
-        x = F.relu(self.deconv4(x))
-        x = torch.cat((x, out_pool1), 1)
-        x = F.relu(self.deconv5(x))
-        x = torch.cat((x, out_pool0), 1)
-        x = self.deconv6(x)
+        x = self.conv3(x)
+        upscore2 = self.deconv1(x)
+
+        # First skip connection
+        score_pool4 = self.conv_0(out_pool4)
+        score_pool4c = self.crop_0(score_pool4)
+        fuse_pool4 = score_pool4c + upscore2
+        upscore_pool4 = self.deconv_0(fuse_pool4)
+
+        # Second skip connection
+        score_pool3 = self.conv_1(out_pool3)
+        score_pool3c = self.crop_1(score_pool3)
+        fuse_pool3 = score_pool3c + upscore_pool4
+        upscore8 = self.deconv_1(fuse_pool3)
+
+        # Crop final result and apply softmax
+        x = self.crop_f(upscore8)
         x = self.softmax(x)
         return x
 
